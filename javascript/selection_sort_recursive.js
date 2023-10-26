fed3eee58129f3b69782cd092b2c943b21748529
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-26 12:24:23 -0400

    done

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,5 +1,15 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+  debugger;
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const min = Math.min(...arr);
+  const minIndex = arr.indexOf(min);
+  arr.splice(minIndex, 1);
+
+  const remaining = selectionSortRecursive(arr);
+  remaining.unshift(min);
+  return remaining;
 }
 
 if (require.main === module) {
@@ -8,9 +18,26 @@ if (require.main === module) {
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
   console.log("");
+
+  console.log("Expecting: [1]");
+  console.log("=>", selectionSortRecursive([1]));
+
+  console.log("Expecting: []");
+  console.log("=>", selectionSortRecursive([]));
+
+  console.log("Expecting: [2, 4, 4, 6, 9]");
+  console.log("=>", selectionSortRecursive([4, 6, 9, 2, 4]));
 }
 
 module.exports = selectionSortRecursive;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+base case: if array length is 1 or less, return that element
+find min and min index of array
+push min onto new array
+splice remove min element from array
+remaining minimum = selectionSortRecursive(arr)
+new array.push(remaining minimum)
+*/
